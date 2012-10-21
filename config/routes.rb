0e73b025e6d9edb039821268c482121f9903c8dd
Brian Egan <bfegan@gmail.com>
2012-10-21 15:36:58 -0400

    made ajax

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Awesome::Application.routes.draw do
+  resources :pirates
+
+  resources :aliens
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +52,7 @@ Awesome::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+root :to => 'pirates#index'
 
   # See how all your routes lay out with "rake routes"
 
